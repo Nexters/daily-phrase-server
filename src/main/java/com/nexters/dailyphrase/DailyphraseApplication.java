53cb52821396c776c4294ba870d70a484f50562f
swa07016 <swa07016@naver.com>
2024-01-16 10:02:42 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/nexters/dailyphrase/DailyphraseApplication.java
@@ -0,0 +1,13 @@
+package com.nexters.dailyphrase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyphraseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyphraseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nexters/dailyphrase/DailyphraseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexters.dailyphrase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyphraseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
