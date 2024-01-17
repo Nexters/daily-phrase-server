8cd1c214ae56abdc6a39919f27ed25707e890a0b
swa07016 <swa07016@naver.com>
2024-01-17 14:43:18 +0900

    feat: MemberApi method signature

--- src/main/java/com/nexters/dailyphrase/member/presentation/dto/MemberResponseDTO.java
+++ src/main/java/com/nexters/dailyphrase/member/presentation/dto/MemberResponseDTO.java
@@ -1,3 +1,23 @@
 package com.nexters.dailyphrase.member.presentation.dto;
 
-public class MemberResponseDTO {}
+import lombok.*;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class MemberResponseDTO {
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class LoginMember {
+        private String field;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ExitMember {
+        private String field;
+    }
+}
