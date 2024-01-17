eee066a7cee43db7a5fc1cc5bea7087a9e5b9925
swa07016 <swa07016@naver.com>
2024-01-17 12:11:04 +0900

    feat: CommonResponse 응답 정의

---
+++ src/main/java/com/nexters/dailyphrase/common/presentation/CommonResponse.java
@@ -0,0 +1,34 @@
+package com.nexters.dailyphrase.common.presentation;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonPropertyOrder({"isSuccess", "code", "message", "result"})
+public class CommonResponse<T> {
+
+    @JsonProperty("isSuccess")
+    private Boolean isSuccess;
+
+    @JsonProperty("code")
+    private String code;
+
+    @JsonProperty("message")
+    private String message;
+
+    @JsonProperty("result")
+    private T result;
+
+    public static <T> CommonResponse<T> onSuccess(T data) {
+        return new CommonResponse<>(true, "요청에 성공하였습니다.", "1000", data);
+    }
+
+    public static <T> CommonResponse<T> onFailure(String code, String message, T data) {
+        return new CommonResponse<>(false, code, message, data);
+    }
+}
