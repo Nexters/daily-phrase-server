aeb7a1b003d8b911414d3bc13b0c76a173521a2a
swa07016 <swa07016@naver.com>
2024-01-17 13:56:03 +0900

    feat: PhraseApi method signature

--- src/main/java/com/nexters/dailyphrase/phrase/presentation/dto/PhraseResponseDTO.java
+++ src/main/java/com/nexters/dailyphrase/phrase/presentation/dto/PhraseResponseDTO.java
@@ -1,4 +1,23 @@
 package com.nexters.dailyphrase.phrase.presentation.dto;
 
+import lombok.*;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class PhraseResponseDTO {
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class PhraseDetail {
+        private String field;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class PhraseList {
+        private String field;
+    }
 }
