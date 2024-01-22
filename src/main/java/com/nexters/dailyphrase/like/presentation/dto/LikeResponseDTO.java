8fc8e00094f57fc51a82b73eacea8fafab978539
swa07016 <swa07016@naver.com>
2024-01-23 01:19:45 +0900

    feat: isLike 필드명 수정

--- src/main/java/com/nexters/dailyphrase/like/presentation/dto/LikeResponseDTO.java
+++ src/main/java/com/nexters/dailyphrase/like/presentation/dto/LikeResponseDTO.java
@@ -2,6 +2,8 @@ package com.nexters.dailyphrase.like.presentation.dto;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.*;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -14,7 +16,11 @@ public class LikeResponseDTO {
     public static class AddLike {
         private Long memberId;
         private Long phraseId;
-        @Builder.Default private boolean isLike = true;
+
+        @Builder.Default
+        @JsonProperty("isLike")
+        private boolean like = true;
+
         private LocalDateTime likedAt;
     }
 
@@ -25,7 +31,11 @@ public class LikeResponseDTO {
     public static class RemoveLike {
         private Long memberId;
         private Long phraseId;
-        private boolean isLike = false;
+
+        @Builder.Default
+        @JsonProperty("isLike")
+        private boolean like = false;
+
         private LocalDateTime canceledAt;
     }
 }
