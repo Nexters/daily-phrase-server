71c9e9707efed555ee8589671be008f034e90296
shincheeun <harusce12@gmail.com>
2024-02-03 04:17:05 +0900

    feat: 관리자 로그인 기능 구현-오타수정

--- src/main/java/com/nexters/dailyphrase/common/exception/GlobalErrorCode.java
+++ src/main/java/com/nexters/dailyphrase/common/exception/GlobalErrorCode.java
@@ -15,7 +15,7 @@ import lombok.Getter;
 public enum GlobalErrorCode implements BaseErrorCode {
     ARGUMENT_NOT_VALID_ERROR(BAD_REQUEST, "GLOBAL_400_1", "잘못된 요청"),
     INTERNAL_SERVER_ERROR(INTERNAL_SERVER, "GLOBAL_500_1", "서버 오류. 관리자에게 문의 부탁드립니다."),
-    TOO_MANY_REQUEST(TOO_MANY_REQUESTS, "GLOBAL_429_1", "과도한 요청을 보내셨습니다. 잠시 기다려 주세요."),
+    TOO_MANY_REQUEST(TOO_MANY_REQUESTS, "GLOBAL_429_1", "과도한 요청을 보내셨습니다. 잠시 기다려 주세요.");
     private final Integer status;
     private final String code;
     private final String reason;
