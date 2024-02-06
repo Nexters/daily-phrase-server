a33fa6d9bef00cc666a59cc0060323e1bb4fc83b
swa07016 <swa07016@naver.com>
2024-02-06 13:55:33 +0900

    feat: SecurityUtils & MemberUtils

---
+++ src/main/java/com/nexters/dailyphrase/common/utils/SecurityUtils.java
@@ -0,0 +1,17 @@
+package com.nexters.dailyphrase.common.utils;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class SecurityUtils {
+
+    public static Long getCurrentMemberId() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null) return 0L;
+        return Long.valueOf(authentication.getName());
+    }
+}
