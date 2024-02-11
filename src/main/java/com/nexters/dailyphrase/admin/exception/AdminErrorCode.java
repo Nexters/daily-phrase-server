e43960c25fb703992e4b32cc4f29eae788865e59
shincheeun <harusce12@gmail.com>
2024-02-11 15:27:56 +0900

    feat:Security Config 예외처리 추가

--- src/main/java/com/nexters/dailyphrase/admin/exception/AdminErrorCode.java
+++ src/main/java/com/nexters/dailyphrase/admin/exception/AdminErrorCode.java
@@ -1,7 +1,6 @@
 package com.nexters.dailyphrase.admin.exception;
 
-import static com.nexters.dailyphrase.common.consts.DailyPhraseStatic.NOT_FOUND;
-import static com.nexters.dailyphrase.common.consts.DailyPhraseStatic.UNAUTHORIZED;
+import static com.nexters.dailyphrase.common.consts.DailyPhraseStatic.*;
 
 import java.lang.reflect.Field;
 import java.util.Objects;
@@ -17,7 +16,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum AdminErrorCode implements BaseErrorCode {
     ADMIN_NOT_FOUND(NOT_FOUND, "ADMIN_404_1", "해당 관리자를 찾을 수 없습니다."),
-    ADMIN_BAD_CREDENTIALS_EXCEPTION(UNAUTHORIZED, "ADMIN_401_1", "자격증명이 없습니다. 아이디 또는 비밀번호가 틀렸습니다.");
+    ADMIN_BAD_CREDENTIALS_EXCEPTION(UNAUTHORIZED, "ADMIN_401_1", "자격증명이 없습니다. 아이디 또는 비밀번호가 틀렸습니다."),
+    ADMIN_UNAUTHORIZED_EXCEPTION(UNAUTHORIZED, "ADMIN_401_2", "유효하지 않은 자격증명입니다."),
+    ADMIN_FORBIDDEN_EXCEPTION(FORBIDDEN, "ADMIN_403_1", "필요한 권한이 없습니다.");
 
     private final Integer status;
     private final String code;
--- src/main/java/com/nexters/dailyphrase/config/SecurityConfig.java
+++ src/main/java/com/nexters/dailyphrase/config/SecurityConfig.java
@@ -1,16 +1,28 @@
 package com.nexters.dailyphrase.config;
 
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Component;
 
+import com.nexters.dailyphrase.admin.exception.AdminErrorCode;
 import com.nexters.dailyphrase.common.jwt.JwtAuthorizationFilter;
 import com.nexters.dailyphrase.common.jwt.JwtTokenService;
 
@@ -22,8 +34,8 @@ import lombok.RequiredArgsConstructor;
 public class SecurityConfig {
 
     private final JwtTokenService jwtTokenService;
-    //    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-    //    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
     private final String[] allowedUrls = {
         "/",
@@ -34,31 +46,54 @@ public class SecurityConfig {
         "/api-docs/**"
     };
 
-    //    @Component
-    //    public static class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Component
+    public static class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
-    //        @Override
-    //        public void commence(
-    //                HttpServletRequest request,
-    //                HttpServletResponse response,
-    //                AuthenticationException authException)
-    //                throws IOException {
-    //
-    //            response.sendError(HttpServletResponse.SC_FORBIDDEN);
-    //        }
-    //    }
-    //
-    //    @Component
-    //    public static class JwtAccessDeniedHandler implements AccessDeniedHandler {
-    //        @Override
-    //        public void handle(
-    //                HttpServletRequest request,
-    //                HttpServletResponse response,
-    //                AccessDeniedException accessDeniedException)
-    //                throws IOException, ServletException {
-    //            response.sendError(HttpServletResponse.SC_FORBIDDEN);
-    //        }
-    //    }
+        @Override
+        public void commence(
+                HttpServletRequest request,
+                HttpServletResponse response,
+                AuthenticationException authException)
+                throws IOException {
+            response.setCharacterEncoding("UTF-8");
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+            AdminErrorCode unauthorizedError = AdminErrorCode.ADMIN_UNAUTHORIZED_EXCEPTION;
+
+            String ErrorResponse =
+                    String.format(
+                            "{\"isSuccess\":\"false\", \"code\":\"%s\", \"message\":\"%s\", \"status\":%d}",
+                            unauthorizedError.getCode(),
+                            unauthorizedError.getReason(),
+                            HttpServletResponse.SC_UNAUTHORIZED);
+
+            response.getWriter().write(ErrorResponse);
+        }
+    }
+
+    @Component
+    public static class JwtAccessDeniedHandler implements AccessDeniedHandler {
+        @Override
+        public void handle(
+                HttpServletRequest request,
+                HttpServletResponse response,
+                AccessDeniedException accessDeniedException)
+                throws IOException, ServletException {
+            response.setCharacterEncoding("UTF-8");
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+
+            AdminErrorCode forbiddenError = AdminErrorCode.ADMIN_FORBIDDEN_EXCEPTION;
+
+            String ErrorResponse =
+                    String.format(
+                            "{\"isSuccess\":\"false\", \"code\":\"%s\", \"message\":\"%s\", \"status\":%d}",
+                            forbiddenError.getCode(),
+                            forbiddenError.getReason(),
+                            HttpServletResponse.SC_FORBIDDEN);
+
+            response.getWriter().write(ErrorResponse);
+        }
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
@@ -66,13 +101,11 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(
                         sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                //                .exceptionHandling(
-                //                        (exception) ->
-                //                                exception
-                //
-                // .authenticationEntryPoint(jwtAuthenticationEntryPoint)
-                //
-                // .accessDeniedHandler(jwtAccessDeniedHandler))
+                .exceptionHandling(
+                        (exception) ->
+                                exception
+                                        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                                        .accessDeniedHandler(jwtAccessDeniedHandler))
                 .authorizeHttpRequests(
                         auth ->
                                 auth.requestMatchers(allowedUrls)
