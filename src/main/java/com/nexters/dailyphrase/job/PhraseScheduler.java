a3279e0a28d45ecd30c9419c85762acbae9fdf79
shincheeun <harusce12@gmail.com>
2024-04-21 21:28:35 +0900

    feat: fcm 알림 개선- 예약된글은 업로드 시점에 알림이 가도록 변경

--- src/main/java/com/nexters/dailyphrase/job/PhraseScheduler.java
+++ src/main/java/com/nexters/dailyphrase/job/PhraseScheduler.java
@@ -1,7 +1,12 @@
 package com.nexters.dailyphrase.job;
 
+import java.io.IOException;
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
+import com.nexters.dailyphrase.notification.SendNotification;
+import com.nexters.dailyphrase.phrase.domain.Phrase;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,10 +20,21 @@ import lombok.RequiredArgsConstructor;
 public class PhraseScheduler {
 
     private final PhraseRepository phraseRepository;
+    private final SendNotification sendNotification;
 
     @Scheduled(cron = "0 0 0 * * *")
     @Transactional
-    public void publishScheduledPhrases() {
-        phraseRepository.updateByIsPublishDate(LocalDate.now());
+    public void publishScheduledPhrases() throws IOException {
+        LocalDate currentDate = LocalDate.now();
+        phraseRepository.updateByIsPublishDate(currentDate);
+
+        List<Phrase> todayPhrase = phraseRepository.findPhraseByPublishDate(currentDate);
+        if (!todayPhrase.isEmpty()) //오늘 예약된 글이 있을때만 알림 전송
+        {
+            Phrase NotificationPhrase =todayPhrase.get(0); // 오늘 업로드예정 글 중 1건만 전송
+            final String alarmBody =  NotificationPhrase.getTitle();
+            final String alarmPhraseId =  NotificationPhrase.getId().toString();
+            sendNotification.sendMessageTo(alarmBody, alarmPhraseId);
+        }
     }
 }
--- src/main/java/com/nexters/dailyphrase/phrase/domain/repository/PhraseRepository.java
+++ src/main/java/com/nexters/dailyphrase/phrase/domain/repository/PhraseRepository.java
@@ -1,6 +1,7 @@
 package com.nexters.dailyphrase.phrase.domain.repository;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -17,6 +18,10 @@ public interface PhraseRepository extends JpaRepository<Phrase, Long>, PhraseCus
     @Query(
             "select p from Phrase p left join fetch p.phraseImage where p.id = :phraseId and p.isReserved = false")
     Optional<Phrase> findPublishPhraseById(Long phraseId);
+
+    @Query(
+            "select p from Phrase p where p.publishDate = :publishDate")
+    List<Phrase> findPhraseByPublishDate(LocalDate publishDate);
 
     @Modifying
     @Query("update Phrase p set p.viewCount = p.viewCount + 1 where p.id = :phraseId")
