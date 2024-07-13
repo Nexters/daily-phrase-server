b4e455e224ab4b61aa2c4db40dd06b93c58d9e1f
swa07016 <swa07016@naver.com>
2024-07-14 01:47:45 +0900

    chore: 타임존 세팅

--- src/main/java/com/nexters/dailyphrase/DailyphraseApplication.java
+++ src/main/java/com/nexters/dailyphrase/DailyphraseApplication.java
@@ -1,5 +1,9 @@
 package com.nexters.dailyphrase;
 
+import java.util.TimeZone;
+
+import jakarta.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -14,6 +18,10 @@ import io.swagger.v3.oas.annotations.servers.Server;
         })
 @EnableScheduling
 public class DailyphraseApplication {
+    @PostConstruct
+    public void setTimeZone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DailyphraseApplication.class, args);
